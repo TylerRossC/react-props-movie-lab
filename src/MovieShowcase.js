61e5e395f0e5a38af867c78ff94dea642f94ba99
tyler <tylercharawell@gmail.com>
2021-05-26 15:16:17 -0700

    commit tree 12208b466591ead45ff611412151c69983b1af98

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,7 +6,13 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map((item, index) => {
+      //return <MovieCard {...item}/>
+      return <MovieCard key = {index} title = {item.title} IMDBRating = {item.IMDBRating} genres = {item.genres} poster = {item.poster} />
+      //why does key = {index} need to be present to stop error in dev tools?
+      })
+    }
+  
 
   render() {
     return (
@@ -16,3 +22,4 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -13,7 +13,6 @@ import React, { Component } from 'react';
 import CardFront from './CardFront.js';
 import CardBack from './CardBack.js';
 
-
 const posterMap = {
   'choux-maru-istanbul': cmi,
   'choux-maru-part-1': cmp1,
@@ -27,17 +26,28 @@ const posterMap = {
   'default': defaultPoster
 }
 
+
 export default class MovieCard extends Component {
+
+//addPoster() {
+//   for (const key in posterMap) {
+//     if(key === this.props.poster){
+//       return posterMap.key
+//     }
+//   }
+// }
 
   render() {
     return (
       <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        {/* <CardFront poster = {this.addPoster()}/> */}
+        <CardFront poster = {posterMap[this.props.poster]} />
+        <CardBack title = {this.props.title} IMDBRating = {this.props.IMDBRating} genres = {this.props.genres}/>
       </div>
     )
   }
 }
 
+
 // Don't forget your default props!
+MovieCard.defaultProps = {title: "Unknown", IMDBRating: null, genres: ['No Genre(s) Found'], poster: "default"}
